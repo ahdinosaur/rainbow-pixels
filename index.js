297416b6bae3ddde424f94a6b4eed049c045cc3a
Michael Williams <michael.williams@enspiral.com>
2016-02-28 22:00:23 +1300

    wip

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
-var inherits = require('inherits')
-var Readable = require('readable-stream').Readable
-var defined = require('defined')
-var Ndarray = require('ndarray')
-var rainbowGradient = require('rainbow-linear-gradient')
+const defined = require('defined')
+const Ndarray = require('ndarray')
+const rainbowGradient = require('rainbow-linear-gradient')
+const infinite = require('pull-stream/sources').infinite
 
 module.exports = RainbowPixels
 
-inherits(RainbowPixels, Readable)
 function RainbowPixels (opts) {
-  if (!(this instanceof RainbowPixels)) {
-    return new RainbowPixels(opts)
-  }
-
   opts = opts || {}
 
-  Readable.call(this, {
-    objectMode: true
-  })
-
-  this.shape = defined(opts.shape, [8, 8]) 
-  this.inc = defined(opts.inc, 5)
-  this.gradientOpts = {
+  var shape = defined(opts.shape, [8, 8])
+  var inc = defined(opts.inc, 5)
+  var gradientOpts = {
     start: 0,
     step: opts.step,
-    length: size(this.shape),
+    length: size(shape),
     saturation: opts.saturation,
-    lightness: opts.lightness
+    lightness: opts.lightness,
+  }
+
+  return infinite(generate)
+
+  function generate () {
+    var gradient = rainbowGradient(gradientOpts)
+
+    gradientOpts.start += inc
+
+    var pixels = Ndarray(
+      gradient.data,
+      shape.concat(gradient.shape.slice(gradient.shape.length - 1))
+    )
+
+    pixels.format = gradient.format
+
+    return pixels
   }
 }
 
-RainbowPixels.prototype._read = read
-RainbowPixels.prototype._readSample = readSample
-
-function read (numSamples) {
-  this.push(this._readSample())
-}
-
-function readSample () {
-  var gradient = rainbowGradient(this.gradientOpts)
-
-  this.gradientOpts.start += this.inc
-
-  var pixels = Ndarray(
-    gradient.data,
-    this.shape.concat(gradient.shape.slice(gradient.shape.length - 1))
-  )
-
-  pixels.format = gradient.format
-
-  return pixels
-}
 
 function size (shape) { return shape.reduce(mult) }
 function mult (a, b) { return a * b }
